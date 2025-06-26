b57f5430e8c4f1b9c3bd8dd020f336d1465e125e
Troy Lorents <tjlorents333@gmail.com>
2025-06-26 13:34:36 -0700

    updated program to allow swagger in prod

--- MyStudentApi/Program.cs
+++ MyStudentApi/Program.cs
@@ -27,12 +27,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 // Use the CORS policy.
 app.UseCors("AllowReactApp");
@@ -45,35 +42,3 @@ app.MapControllers();
 
 app.Run();
 
-
-
-
-//using Microsoft.EntityFrameworkCore;
-//using MyStudentApi.Data;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllers();
-
-//// Register EF Core and configure your database connection.
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-//// Register Swagger services.
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI(); // You can pass options here if needed.
-//}
-
-//app.UseHttpsRedirection();
-//app.UseAuthorization();
-//app.MapControllers();
-//app.Run();
